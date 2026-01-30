7d101805c552a82521b624ac03bcebbddbe1df13
Artemii Bigdan <artem@bigdan.in>
2026-01-30 19:00:40 +0100

    plugin needs a constructor

--- main.go
+++ main.go
@@ -1,3 +1,14 @@
 package main
 
-import _ "github.com/UQuark/floatmoneylint/lib"
+import (
+	"github.com/UQuark/floatmoneylint/lib"
+	"golang.org/x/tools/go/analysis"
+)
+
+func New(settings any) ([]*analysis.Analyzer, error) {
+	plugin, err := lib.New(settings)
+	if err != nil {
+		return nil, err
+	}
+	return plugin.BuildAnalyzers()
+}
